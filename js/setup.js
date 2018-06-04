7d1d63f31bc0a239651cee3fe89ee4e3bec3e1ae
Dorofeev Aleksey <adorofeev@ozon>
2018-06-04 17:48:14 +0300

    задание 1

---
+++ js/setup.js
@@ -0,0 +1,54 @@
+'use strict';
+
+
+(function () {
+  var userDialog = document.querySelector('.setup');
+  var similarListElement = userDialog.querySelector('.setup-similar-list');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+  var WIZARDS_COUNT = 4;
+  var wizards = [];
+
+  for (var i = 0; i < 4; i++) {
+    wizards[i] = {
+      name: window.util.getRandomItem(window.util.WIZARD_NAMES) + ' ' + window.util.getRandomItem(window.util.WIZARD_SURNAME),
+      coatColor: window.util.getRandomItem(window.util.COLOR_COAT),
+      eyesColor: window.util.getRandomItem(window.util.COLOR_EYES)
+    };
+  }
+
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
+
+    return wizardElement;
+  };
+  var successHandler = function (regularWizards) {
+    var fragment = document.createDocumentFragment();
+    for (i = 0; i < WIZARDS_COUNT; i++) {
+      fragment.appendChild(renderWizard(window.util.getRandomItem(regularWizards)));
+    }
+    similarListElement.appendChild(fragment);
+  };
+
+  window.backend.load(successHandler, window.backend.errorHandler);
+  // Смена цветов частей волшебника по клику
+
+  var wizardCoat = document.querySelector('.setup-wizard .wizard-coat');
+  var wizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
+  var fireballWrap = document.querySelector('.setup-fireball-wrap');
+
+  var fillElement = function (element, color) {
+    element.style.fill = color;
+  };
+
+  var changeElementBackground = function (element, color) {
+    element.style.backgroundColor = color;
+  };
+
+  window.colorizeElement(wizardCoat, window.util.COLOR_COAT, fillElement);
+  window.colorizeElement(wizardEyes, window.util.COLOR_EYES, fillElement);
+  window.colorizeElement(fireballWrap, window.util.COLOR_FIREBALLS, changeElementBackground);
+})();
